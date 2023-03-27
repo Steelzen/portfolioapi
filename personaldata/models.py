a891c74f4c8029eec95c0bccf4c6c82e1e6ebd14
Taehyung Kwon <katalogin88@gmail.com>
2023-03-27 23:27:14 +0100

    add and modify models

--- personaldata/models.py
+++ personaldata/models.py
@@ -7,21 +7,23 @@ class AboutMe(models.Model):
     phone = models.CharField(max_length=30)
     site = models.URLField(max_length=300, default="")
     github = models.URLField(max_length=300, default="")
-    summary = models.CharField(max_length=300)
+    summary = models.CharField(max_length=500, default="")
     photo_src = models.URLField(max_length=300, default="")
-    introductory = models.CharField(max_length=300, default="")
+    introductory = models.CharField(max_length=500, default="")
     title_img_src = models.URLField(max_length=300, default="")
+    additional_info = models.CharField(max_length=500, default="")
 
-    
     def __str__(self):
         return self.name
 
 class Project(models.Model):
     name = models.CharField(max_length=50)
-    content = models.CharField(max_length=300)
+    content = models.CharField(max_length=500, default="")
     img_src = models.URLField(max_length=300, default="")
     site_link = models.URLField(max_length=300, default="")
     code_link = models.URLField(max_length=300, default="")
+    video_link = models.URLField(max_length=300, default="")
+    additional_info = models.CharField(max_length=500, default="")
     
     def __str__(self):
         return self.name      
